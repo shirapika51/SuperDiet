95c37e103ae89708d2fafdac23b9a9ee6514ec48
Shirapika <shirapika51@gmail.com>
2018-10-25 16:23:00 +0300

    'addingpictures'

---
+++ SuperDiet/Migrations/20181024091313_trydbcontext2.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SuperDiet.Migrations
+{
+    public partial class trydbcontext2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Item_Order_OrderID",
+                table: "Item");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Order_User_UserID",
+                table: "Order");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_User",
+                table: "User");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Order",
+                table: "Order");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Item",
+                table: "Item");
+
+            migrationBuilder.RenameTable(
+                name: "User",
+                newName: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Order",
+                newName: "Orders");
+
+            migrationBuilder.RenameTable(
+                name: "Item",
+                newName: "Items");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Order_UserID",
+                table: "Orders",
+                newName: "IX_Orders_UserID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Item_OrderID",
+                table: "Items",
+                newName: "IX_Items_OrderID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "ID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders",
+                column: "ID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Items",
+                table: "Items",
+                column: "ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Orders_OrderID",
+                table: "Items",
+                column: "OrderID",
+                principalTable: "Orders",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Users_UserID",
+                table: "Orders",
+                column: "UserID",
+                principalTable: "Users",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Orders_OrderID",
+                table: "Items");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Users_UserID",
+                table: "Orders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Items",
+                table: "Items");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "User");
+
+            migrationBuilder.RenameTable(
+                name: "Orders",
+                newName: "Order");
+
+            migrationBuilder.RenameTable(
+                name: "Items",
+                newName: "Item");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Orders_UserID",
+                table: "Order",
+                newName: "IX_Order_UserID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Items_OrderID",
+                table: "Item",
+                newName: "IX_Item_OrderID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_User",
+                table: "User",
+                column: "ID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Order",
+                table: "Order",
+                column: "ID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Item",
+                table: "Item",
+                column: "ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Item_Order_OrderID",
+                table: "Item",
+                column: "OrderID",
+                principalTable: "Order",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Order_User_UserID",
+                table: "Order",
+                column: "UserID",
+                principalTable: "User",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ SuperDiet/Models/Branch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperDiet.Models
+{
+    public class Branch
+    {
+        public int ID { get; set; }
+        public String City { get; set; }
+        public String Address { get; set; }
+
+    }
+}
